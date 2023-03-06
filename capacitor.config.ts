d3537b6717a52bde8293c7cb1448883e491df22f
Damian Tarnawsky <damian@ionic.io>
2023-03-06 06:24:17 -0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.ac3.sync',
+  appName: 'cs-ac3-sync',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
